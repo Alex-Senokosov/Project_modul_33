32f04e618222437d8030503b3dc0e1d97eca4323
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-24 20:33:17 +0300

    DZ3

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
+
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def index():
-    return " Hello World"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return " About US"
+    return render_template("about.html")
 
-@app.route("/user/<string:name>>/<int:id>")
+@app.route("/user/<string:name>/<int:id>")
 def user(name,id):
-    return "  User page"+ name + "-"+ id
+    return "User page:"+ name + "-"+ str(id)
 
 
 if __name__=="__main__":
