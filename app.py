1c1f3596fa3d1ef47cd52629fb431d3b0d6569d7
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-25 20:52:31 +0300

    DZ3

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask,render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+db = SQLAlchemy(app)
+class Article (db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300),nullable= False)
+    text = db.Column(db.Text,nullable= False)
+    date = db.Column(db.DateTime,default= datetime.utcnow)
+    def __repr__(self):
+        return '<Article %r>' %self.id
+
+
+
+
 
 @app.route("/")
 @app.route("/home")
@@ -11,9 +27,10 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/user/<string:name>/<int:id>")
-def user(name,id):
-    return "User page:"+ name + "-"+ str(id)
+@app.route("/create-article")
+def create_article():
+    return render_template("create_article.html")
+
 
 
 if __name__=="__main__":
